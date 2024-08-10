b238b4a3e0885b6adaeef8a51f95cc02078c4cce
rwor7hless <rwor7hless@gmail.com>
2024-08-10 23:22:30 +0300

    added some classes
    Added: Assets and Implemented Game classes

--- rpg-like/rpg-like/src/Game.cpp
+++ rpg-like/rpg-like/src/Game.cpp
@@ -1,1 +1,45 @@
 #include "Game.h"
+
+
+Game::Game() { init("123"); }
+
+
+void Game::init(std::string path)
+{   
+    //TODO: 
+    //  1.implemetn loadFromFile func: m_assets.loadFromFile(path);
+    //  
+	m_window.create(sf::VideoMode(1280, 768), "RPG_LIKE");
+}
+
+void Game::sUserInput()
+{
+
+}
+
+
+
+void Game::render()
+{
+    m_window.clear();
+    // Отрисовка объектов игры
+    m_window.display();
+}
+
+void Game::run()
+{
+    while (isRunning()) {
+        sUserInput();
+        update();
+        render();
+    }
+}
+
+void Game::update()
+{
+}
+
+bool Game::isRunning()
+{
+    return m_running && m_window.isOpen();
+}
--- rpg-like/rpg-like/src/Game.h
+++ rpg-like/rpg-like/src/Game.h
@@ -1,5 +1,32 @@
-#pragma once
+#ifndef GAME_H
+#define GAME_H
+#include <SFML/Graphics.hpp>
+#include "Assets.h"
+#include <memory>
 class Game
 {
+protected:
+	//Variables
+	sf::RenderWindow	m_window;
+	sf::Event			m_event;
+	std::string			m_currentScene;
+	bool				m_running = true;
+	Assets				m_assets;
+
+	//Initialization
+	void init(std::string path);
+	void sUserInput();
+public:
+	//Constructor
+	Game();
+
+
+	//Functions
+	void render();
+	void run();
+	void update();
+	bool isRunning();
+
 };
+#endif // !GAME_H
 
